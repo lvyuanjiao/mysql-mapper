475ecb5760e6b62cc8c48c3fd88dbb997246eae8
lvyuanjiao <lvyuanjiao@lvyuanjiaos-MacBook-Air.local>
2017-02-08 20:37:41 +0800

    Update

--- test/test.js
+++ test/test.js
@@ -23,50 +23,43 @@ var post = {
 };
 
 describe('MySQL Mapper', function() {
-
   before(function(done) {
-    mapper.create(path.join(__dirname, 'mappers'), opts, function(err, mapper) {
-      should.not.exist(err);
+    mapper.create(path.join(__dirname, 'mappers'), opts).then(function(mapper){
       should.exist(mapper);
       done();
-    });
+    }).catch(done);
   });
 
   describe('Query', function() {
-
     it('#insert()', function(done) {
-      mapper().insert('post.insert', post, function(err, insertId) {
-        should.not.exist(err);
+      mapper().insert('post.insert', post).then(function(insertId) {
         insertId.should.be.a.Number();
         post.id = insertId;
         done();
-      });
+      }).catch(done);
     });
 
     it('#update()', function(done) {
       post.updated = new Date();
-      mapper().update('post.update', post, function(err, affectedRows) {
-        should.not.exist(err);
+      mapper().update('post.update', post).then(function(affectedRows) {
         affectedRows.should.be.above(0);
         done();
-      });
+      }).catch(done);
     });
 
     it('#selectOne()', function(done) {
-      mapper().selectOne('post.selectById', post.id, function(err, ret) {
-        should.not.exist(err);
+      mapper().selectOne('post.selectById', post.id).then(function(ret) {
         ret.should.be.instanceof(Object).and.have.properties({
           title: post.title,
           content: post.content,
           author: post.author
         });
         done();
-      });
+      }).catch(done);
     });
 
     it('#select()', function(done) {
-      mapper().select('post.selectAll', function(err, posts) {
-        should.not.exist(err);
+      mapper().select('post.selectAll').then(function(posts) {
         posts.should.be.instanceof(Array);
         should.exist(posts[0]);
         posts[0].should.have.properties({
@@ -75,88 +68,62 @@ describe('MySQL Mapper', function() {
           author: post.author
         });
         done();
-      });
+      }).catch(done);
     });
 
     it('#delete()', function(done) {
-      mapper().delete('post.deleteById', post.id, function(err, affectedRows) {
-        should.not.exist(err);
+      mapper().delete('post.deleteById', post.id).then(function(affectedRows) {
         affectedRows.should.be.above(0);
         done();
-      });
+      }).catch(done);
     });
-
   });
 
   describe('Transaction', function() {
-
-    it('#tasks as array', function(done) {
+    it('#commit', function(done) {
       var testMapper = mapper();
-      testMapper.transaction([
-        function(next, conn, map) {
-          testMapper.insert('post.insert', post, next, conn);
-        },
-        function(next, conn, map) {
-          post.id = map[0];
-          post.updated = new Date();
-          testMapper.update('post.update', post, next, conn);
-        },
-        function(next, conn, map) {
-          testMapper.delete('post.deleteById', post.id, next, conn);
-        }
-      ], function(err, map) {
-        should.not.exist(err);
-        map.should.be.instanceof(Array).and.have.lengthOf(3);
-        map[0].should.be.a.Number();
-        map[1].should.be.above(0);
-        map[2].should.be.ok();
-        done();
-      });
-    });
-
-    it('#tasks as object', function(done) {
-      var testMapper = mapper();
-      testMapper.transaction({
-        insertId: function(next, conn, map) {
-          testMapper.insert('post.insert', post, next, conn);
-        },
-        updateRows: function(next, conn, map) {
-          post.id = map.insertId;
-          post.updated = new Date();
-          testMapper.update('post.update', post, next, conn);
-        },
-        deleteRows: function(next, conn, map) {
-          testMapper.delete('post.deleteById', post.id, next, conn);
-        }
-      }, function(err, map) {
-        should.not.exist(err);
-        map.should.be.instanceof(Object);
-        map.insertId.should.be.a.Number();
-        map.updateRows.should.be.above(0);
-        map.deleteRows.should.be.ok();
-        done();
-      });
+      testMapper.begin().then(function(conn) {
+        testMapper.insert(conn, 'post.insert', post)
+          .then(function(postId) {
+            postId.should.be.a.Number();
+            post.id = postId;
+            post.updated = new Date();
+            return testMapper.update(conn, 'post.update', post);
+          })
+          .then(function(changedRows){
+            changedRows.should.be.above(0);
+            return testMapper.delete(conn, 'post.deleteById', post.id);
+          })
+          .then(function(affectedRows){
+            affectedRows.should.be.ok();
+            return testMapper.commit(conn);
+          })
+          .then(done)
+          .catch(done);
+      }).catch(done);
     });
 
     it('#rollback', function(done) {
-      var testMapper = mapper('blog');
-      testMapper.transaction({
-        insertId: function(next, conn, map) {
-          testMapper.insert('post.insert', post, next, conn);
-        },
-        // Throw error
-        errorQuery: function(next, conn, map) {
-          post.id = map.insertId;
-          testMapper.select('post.errorQuery', next, conn);
-        },
-        deleteRows: function(next, conn, map) {
-          testMapper.delete('post.deleteById', post.id, next, conn);
-        }
-      }, function(err, map) {
-        should.exist(err);
-        done();
-      });
+      var testMapper = mapper();
+      testMapper.begin().then(function(conn) {
+        testMapper.insert(conn, 'post.insert', post)
+          .then(function(postId) {
+            postId.should.be.a.Number();
+            post.id = postId;
+            post.updated = new Date();
+            return testMapper.select(conn, 'post.errorQuery', post);
+          })
+          .then(function(){
+            return testMapper.commit(conn);
+          })
+          .then(done)
+          .catch(function(err){
+            should.exist(err);
+            testMapper.rollback(conn).then(function(){
+              done();
+            });
+          });
+      }).catch(done);
     });
-
   });
 });
